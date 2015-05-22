9658bf0b6f773fef3c1a0fb6ef4007904dce51ae
cole.donovan <cole.donovan@MU-C3-10.mullen.pvt.k12.co.us>
2015-05-22 08:19:53 -0600

    go

---
+++ Array Challenges/src/ArrayChallenges.java
@@ -0,0 +1,34 @@
+
+public class ArrayChallenges
+	{
+
+	public static void main(String[] args)
+		{
+		int i;
+		int m;
+		double total=0;
+		int[] challenge1 = {3,72,89,54,12};
+		System.out.println(challenge1[2]);
+		
+		int[] challenge2 = {7,31,0,62,57};
+		
+		for(i = 0; i<challenge2.length;i++)
+			{
+				
+			}
+		System.out.println(challenge2[i-1]);
+		
+		for(int x = 0; x<challenge2.length; x++)
+			{
+				System.out.println(challenge2[x]);
+			}
+		for( m=0; m<challenge2.length; m++)
+			{
+				total=total+challenge2[m];
+			}
+		System.out.println(total/challenge2.length);
+		
+
+		}
+
+	}
